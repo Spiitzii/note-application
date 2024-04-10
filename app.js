eef581034adf41643feec1060a1ed83730bafbf6
Spiitzii <spitzi@online.de>
2024-04-10 12:38:19 +0200

    functional Menu

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 import { addNote, showNotes, deleteNote} from './helpers.js';
 import inquirer from 'inquirer';
+import { UserMenu } from './helpers.js';
 
-function UserMenu(userInput) {
-    switch(userInput) {
-        case 'add':
-            // addNote-Funktion starten
-            addNote();
-            break;
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'Auswahl',
+        message: 'was möchten Sie tun?',
+        choices: ['add', 'show', 'delete']
+    }
+]).then((answers) => {
+    // Hier wird die Benutzereingabe erfasst
+    const userInput = answers.Auswahl.trim(); // Erfasse die Benutzereingabe und entferne Leerzeichen
+
+    if (userInput === 'add' || userInput === 'show' || userInput === 'delete') {
+        // Gültige Auswahl, rufe UserMenu mit userInput auf
+        UserMenu(userInput);
+    } else {
+        console.error('Ungültige Auswahl:', userInput);
+    }
+});
+
